c2b9efe6d8d9f6147899880ce2b137fe905cfa01
parisa <parisa.abbas@gmail.com>
2026-09-16 19:54:11 +0800

    Created user account database model

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,31 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, Float, Integer
+from sqlalchemy import DateTime, Float, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+
+    email: Mapped[str] = mapped_column(
+        String,
+        unique=True,
+        index=True,
+        nullable=False
+    )
+
+    password_hash: Mapped[str] = mapped_column(
+        String,
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
 
 class Calculation(Base):
     __tablename__ = "calculations"
