cd9505d05893b6ea85532767a0ae2ea8f30bebec
parisa <parisa.abbas@gmail.com>
2026-09-24 22:20:55 +0800

    Configured production CORS

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,10 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://carbon-emission-ai.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
